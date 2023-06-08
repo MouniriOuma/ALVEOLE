35cd89154ceafcb000de6ed635b28be4a5508245
MouniriOuma <mouniri.ouma@gmail.com>
2023-06-08 09:19:32 +0100

    getting the User Role By their Username

--- src/main/java/com/alveole/controller/UserController.java
+++ src/main/java/com/alveole/controller/UserController.java
@@ -4,7 +4,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.Set;
 
+import com.alveole.model.ERole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import com.alveole.exception.ResourceNotFoundException;
 import com.alveole.model.User;
 import com.alveole.repository.UserRepository;
+import com.alveole.model.Role;
 
 
 
@@ -40,6 +44,32 @@ public class UserController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @GetMapping("role/{username}")
+    public ResponseEntity<?> getUserRoleByUsername(@PathVariable String username) {
+        User user = userRepository.findByUsername(username)
+                .orElse(null);
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Set<Role> roles = user.getRoles();
+
+        if (roles.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        List<String> roleNames = roles.stream()
+                .map(role -> role.getName().toString()) // Convert ERole to String
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(roleNames);
+    }
+
+
+
+
 
 
     @DeleteMapping("/{id}")
